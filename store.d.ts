b6d55c1c8df51013c9a61b26f4ad950bc71c6a96
Brian Takita <brian.takita@gmail.com>
2020-01-04 19:02:32 -0500

    feat: store: b__ factories

--- store.d.ts
+++ store.d.ts
@@ -1,9 +1,1 @@
-export declare const __path__csv: import("svelte/store").Writable<any>;
-declare type Opts__load__data__csv = {
-    path__csv?: string;
-    table?: [][];
-    domain__table?: [][];
-    domain__ticks?: [];
-};
-export declare function load__data__csv(opts?: Opts__load__data__csv): Promise<unknown>;
-export {};
+export declare const b__path__csv: (ctx?: any, opts?: any) => any;
--- store.ts
+++ store.ts
@@ -1,9 +1,9 @@
 import { get, writable } from 'svelte/store'
+import { assign, _b } from '@ctx-core/object'
 import {
-	__columns__data,
-	__domain__table,
-	__domain__ticks,
-	__table,
+	b__domain__table,
+	b__domain__ticks,
+	b__table,
 } from '@ctx-core/table/store'
 import { fetch } from '@ctx-core/fetch'
 import Papa from 'papaparse'
@@ -14,42 +14,49 @@ import {
 } from './lib'
 import { info, log } from '@ctx-core/logger'
 const logPrefix = '@ctx-core/csv/store.js'
-export const __path__csv = writable(null)
 type Opts__load__data__csv = {
 	path__csv?:string
 	table?:[][]
 	domain__table?:[][]
 	domain__ticks?:[]
 }
-export function load__data__csv(opts:Opts__load__data__csv = {}) {
-	log(`${logPrefix}|load__data__csv`)
-	const path__csv = opts.path__csv || get(__path__csv)
-	let table = opts.table || get(__table)
-	let domain__table = opts.domain__table || get(__domain__table)
-	let domain__ticks = opts.domain__ticks || get(__domain__ticks)
-	return new Promise(
-		resolve=>{
-			log(`${logPrefix}|load__data__csv|Promise`)
-			// TODO: move to a web worker
-			setTimeout(()=>{
-				info(`${logPrefix}|load__data__csv|Promise|setTimeout`)
-				if (!table && path__csv) {
-					(async ()=>{
-						log(`${logPrefix}|load__data__csv|Promise|setTimeout|path__csv`, path__csv)
-						const response = await fetch(path__csv)
-						const text = await response.text()
-						table = Papa.parse(text).data
-						const columns = table[0]
-						const rows = table.slice(1)
-						cast__rows(rows, columns)
-						push__row_id__i(rows, columns)
-						__table.set(table)
-						__domain__table.set(domain__table)
-						__domain__ticks.set(domain__ticks)
-						// wait for agent change events to propagate
-						subscribe__change__once(__table, resolve)
-					})()
-				}
-			}, 0)
-		})
-}
+export const b__path__csv = _b('__path__csv', ctx=>{
+	const __path__csv = writable(null)
+	return assign(__path__csv, {
+		load__data__csv,
+	})
+	function load__data__csv(opts:Opts__load__data__csv = {}) {
+		log(`${logPrefix}|load__data__csv`)
+		const path__csv = opts.path__csv || get(b__path__csv(ctx))
+		let table = opts.table || get(b__table(ctx))
+		let domain__table =
+			opts.domain__table || get(b__domain__table(ctx))
+		let domain__ticks =
+			opts.domain__ticks || get(b__domain__ticks(ctx))
+		return new Promise(
+			resolve=>{
+				log(`${logPrefix}|load__data__csv|Promise`)
+				// TODO: move to a web worker
+				setTimeout(()=>{
+					info(`${logPrefix}|load__data__csv|Promise|setTimeout`)
+					if (!table && path__csv) {
+						(async ()=>{
+							log(`${logPrefix}|load__data__csv|Promise|setTimeout|path__csv`, path__csv)
+							const response = await fetch(path__csv)
+							const text = await response.text()
+							table = Papa.parse(text).data
+							const columns = table[0]
+							const rows = table.slice(1)
+							cast__rows(rows, columns)
+							push__row_id__i(rows, columns)
+							b__table(ctx).set(table)
+							b__domain__table(ctx).set(domain__table)
+							b__domain__ticks(ctx).set(domain__ticks)
+							// wait for agent change events to propagate
+							subscribe__change__once(b__table(ctx), resolve)
+						})()
+					}
+				}, 0)
+			})
+	}
+})
