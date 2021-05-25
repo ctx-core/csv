b57c1d3fa06f29a08b8fa904807c4fe8b83c8b39
Brian Takita <brian.takita@gmail.com>
2021-05-25 01:29:12 -0400

    ## 8.4.0
    ### Minor Changes
    
    - fix tsc build directory issues
    
    ### Patch Changes
    
    - Updated dependencies [undefined]
    - @ctx-core/array@20.5.0
    - @ctx-core/combinators@6.3.0
    - @ctx-core/fetch@8.3.0
    - @ctx-core/function@17.11.0
    - @ctx-core/object@18.2.0
    - @ctx-core/store@24.10.0
    - @ctx-core/table@9.4.0

--- src/column_name_toLowerCase.js
+++
@@ -1,8 +0,0 @@
-export function column_name_toLowerCase(csv) {
-    const csv_row_a1 = csv.split('\n');
-    const out_csv = [csv_row_a1[0].toLowerCase(),
-        ...csv_row_a1.slice(1)
-    ].join('\n');
-    return out_csv;
-}
-export { column_name_toLowerCase as toLowerCase__column_name, column_name_toLowerCase as toLowerCase__column_name__csv, };
